63a53e28e3ac1413610332f621881790ac4a78a6
Mihad choudhury <cmihad@gmail.com>
2023-08-03 20:48:15 +0100

    ver 1.5 added the url parser and the data from req body now can be accessed

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
 const express = require('express')
 const app = express()
 require('dotenv').config()
+
+// parse application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(express.json())
+
 const user = require('./src/Models/User')
-const Users = require('./src/Routes/User')
+
 const db = require('./src/Database/Data')
+
 app.get('/', (req, res) => {
   res.send('Hello World')
-  console.log(db)
+})
+
+app.post('/zone', async (req, res) => {
+  console.log(req.body)
 })
 
 app.post('/test', async (req, res) => {
-  // const { name, email, password } = req.body
+  const { name, email, password } = req.body
   res.send('working')
 
   try {
