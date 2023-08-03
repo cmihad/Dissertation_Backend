1e39f1831c4f02106f432a3f68094e956093fb9f
Mihad choudhury <cmihad@gmail.com>
2023-08-03 01:37:54 +0100

    ver 1.1 express added and nodemon added

--- index.js
+++ index.js
@@ -1,19 +1,8 @@
-// Importing http core package/module provided by nodejs
+const express = require('express')
+const app = express()
 
-var http = require('http')
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
 
-// Creating a server
-
-http
-  .createServer(function (req, res) {
-    // Write response as Html(text)
-
-    res.writeHead(200, { 'Content-Type': 'text/html' })
-
-    // Writing static text
-
-    res.end('Hello World')
-
-    // Server listening on port number 4200
-  })
-  .listen(4200)
+app.listen(3000)
