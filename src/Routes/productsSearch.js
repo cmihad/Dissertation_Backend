85247649f9fce53d25a278c165fd3973e0cedf49
Mihad choudhury <cmihad@gmail.com>
2023-09-27 18:25:01 +0100

    updated

--- src/Routes/productsSearch.js
+++ src/Routes/productsSearch.js
@@ -2,9 +2,22 @@ const express = require('express')
 const axios = require('axios')
 const cheerio = require('cheerio')
 const { errors } = require('pg-promise')
+const puppeteer = require('puppeteer')
 
 const secret = process.env.JWT_SECRET
 const router = express.Router()
+async function fetchDataWithPuppeteer(url) {
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
+  await page.goto(url)
+
+  // Make sure to wait for any client-side JavaScript to execute
+  await page.waitForTimeout(3000)
+
+  const html = await page.content()
+  await browser.close()
+  return html
+}
 
 router.post('/tesco', async (req, res) => {
   const url = req.body.url
@@ -34,9 +47,14 @@ async function extractData(url) {
   const $ = cheerio.load(html)
 
   const products = []
-
+  const allImages = []
+  $('img.product-image').each(function () {
+    allImages.push($(this).attr('src'))
+  })
+  // console.log(html)
   $('.product-details--wrapper').each((index, element) => {
-    const productImage = $('.product-image__container img').attr('src')
+    const productImage = allImages[index]
+
     const productName = $(element).find('h3').text()
     const price = $(element).find('p').text()
     const pricePerUnit = $(element).find('p').text()
@@ -57,54 +75,104 @@ async function extractData(url) {
   return products
 }
 
+// router.get('/sainsbury', async (req, res) => {
+//   const url = req.body.url
+//   const data1 = fetchDataWithPuppeteer(url)
+//   console.log(data1, 'data 1 this is')
+//   async function fetchData(url) {
+//     try {
+//       const response = await axios.get(url)
+//       return response.data
+//     } catch (error) {
+//       console.error('Error fetching the data', error)
+//     }
+//   }
+
+//   async function extractProductDataFromURL(url) {
+//     const html = await fetchData(url)
+//     const $ = cheerio.load(html)
+//     console.log(html)
+
+//     const products = []
+
+//     // For each product item in the list
+//     $('li.pt-grid-item').each((index, element) => {
+//       const productName = $(element)
+//         .find('h2.pt__info__description a.pt__link')
+//         .text()
+//         .trim()
+//       const productPrice = $(element)
+//         .find('.pt__cost__retail-price')
+//         .text()
+//         .trim()
+//       const productUnitPrice = $(element)
+//         .find('.pt__cost__unit-price-per-measure')
+//         .text()
+//         .trim()
+
+//       products.push({
+//         productName,
+//         productPrice,
+//         productUnitPrice,
+//       })
+//     })
+
+//     return products
+//   }
+
+//   extractProductDataFromURL(url).then((data) => {
+//     console.log('hellop')
+//     console.log(JSON.stringify(data, null, 2))
+//   })
+// })
+
 router.get('/sainsbury', async (req, res) => {
   const url = req.body.url
-  async function fetchData(url) {
-    try {
-      const response = await axios.get(url)
-      return response.data
-    } catch (error) {
-      console.error('Error fetching the data', error)
-    }
-  }
 
-  async function extractProductDataFromURL(url) {
-    const html = await fetchData(url)
-    const $ = cheerio.load(html)
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
 
-    const products = []
+  // Navigate to the page
+  await page.goto(url)
 
-    // For each product item in the list
-    $('li.pt-grid-item').each((index, element) => {
-      const productName = $(element)
-        .find('h2.pt__info__description a.pt__link')
-        .text()
-        .trim()
-      const productPrice = $(element)
-        .find('.pt__cost__retail-price')
-        .text()
-        .trim()
-      const productUnitPrice = $(element)
-        .find('.pt__cost__unit-price-per-measure')
-        .text()
-        .trim()
+  // Optionally, wait for some selector to be loaded
+  await page.waitForSelector('li.pt-grid-item')
 
-      products.push({
-        productName,
-        productPrice,
-        productUnitPrice,
-      })
+  // Get HTML content after JS has been executed
+  const html = await page.content()
+
+  await browser.close()
+
+  const $ = cheerio.load(html)
+
+  const products = []
+
+  // For each product item in the list
+  $('li.pt-grid-item').each((index, element) => {
+    const productName = $(element)
+      .find('h2.pt__info__description a.pt__link')
+      .text()
+      .trim()
+    const productPrice = $(element)
+      .find('.pt__cost__retail-price')
+      .text()
+      .trim()
+    const productUnitPrice = $(element)
+      .find('.pt__cost__unit-price-per-measure')
+      .text()
+      .trim()
+
+    products.push({
+      productName,
+      productPrice,
+      productUnitPrice,
     })
-
-    return products
-  }
-
-  extractProductDataFromURL(url).then((data) => {
-    console.log('hellop')
-    console.log(JSON.stringify(data, null, 2))
   })
-})
 
+  console.log(JSON.stringify(products, null, 2))
+  // You can also send the result back to the client
+  res.json(products)
+})
 router.post('/superdrug', async (req, res) => {
   try {
     const url = req.body.url
@@ -134,8 +202,6 @@ router.post('/superdrug', async (req, res) => {
       products.push(product)
     })
 
-    console.log(products)
-
     res.json(products)
   } catch (error) {
     console.error('Error fetching the URL:', error)
@@ -146,71 +212,99 @@ router.post('/superdrug', async (req, res) => {
 router.get('/boots', async (req, res) => {
   const url = req.body.url // Replace with the actual URL
 
-  async function fetchProductDetails(url) {
-    try {
-      const headers = {
-        'User-Agent':
-          'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
-        Accept:
-          'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-      }
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
 
-      const response = await axios.get(url, { headers })
-      const htmlContent = response.data
-      const $ = cheerio.load(htmlContent)
+  // Navigate to the page
+  await page.goto(url)
 
-      const products = []
+  // Wait for the dynamic content to load
+  await page.waitForSelector('.ais-Hits-item')
 
-      $('.estore_product_container').each((index, element) => {
-        const productDetails = {}
-        const productElement = $(element)
+  // Get the rendered HTML
+  const content = await page.content()
 
-        productDetails.productName = productElement
-          .find('.product_name a')
-          .attr('title')
-        productDetails.productLink = productElement
-          .find('.product_name a')
-          .attr('href')
-        productDetails.productImgLink = productElement
-          .find('.product_image a')
-          .attr('href')
-        productDetails.productImgSrc = productElement
-          .find('.product_image img')
-          .attr('src')
-        productDetails.productImgAlt = productElement
-          .find('.product_image img')
-          .attr('alt')
-        productDetails.rating = productElement
-          .find('.product_rating .rating5')
-          .attr('title')
-        productDetails.reviewCount = productElement
-          .find('.product_review_count')
-          .text()
-          .replace(/[\(\)]/g, '')
-          .trim()
-        productDetails.price = productElement
-          .find('.product_price')
-          .text()
-          .trim()
-        productDetails.pricePerUnit = productElement
-          .find('.price')
-          .text()
-          .trim()
-        productDetails.productId = productElement.attr('data-productid')
-        productDetails.objectId = productElement.attr('data-object-id')
+  // Close the browser
+  await browser.close()
 
-        products.push(productDetails)
-      })
+  // Load content into cheerio
+  const $ = cheerio.load(content)
+  // Extract the data
+  const name = $('.ais-Hits-item .offerName').text() // Replace .offerName with the actual class or ID that contains the name
+  const price = $('.ais-Hits-item .currentPrice').text() // Replace .currentPrice with the actual class or ID that contains the price
 
-      return products
-    } catch (error) {
-      console.error('Error fetching the URL:', error)
-      return null
-    }
-  }
-
-  fetchProductDetails(url).then((products) => {
-    console.log(products)
-  })
+  console.log(`Name: ${name}`)
+  console.log(`Price: ${price}`)
 })
+// router.get('/boots', async (req, res) => {
+//   const url = req.body.url // Replace with the actual URL
+
+//   async function fetchProductDetails(url) {
+//     try {
+//       const headers = {
+//         'User-Agent':
+//           'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+//         Accept:
+//           'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+//       }
+
+//       const response = await axios.get(url, { headers })
+//       const htmlContent = response.data
+//       const $ = cheerio.load(htmlContent)
+//       console.log(htmlContent, 'check the html')
+
+//       const products = []
+
+//       $('.estore_product_container').each((index, element) => {
+//         const productDetails = {}
+//         const productElement = $(element)
+
+//         productDetails.productName = productElement
+//           .find('.product_name a')
+//           .attr('title')
+//         productDetails.productLink = productElement
+//           .find('.product_name a')
+//           .attr('href')
+//         productDetails.productImgLink = productElement
+//           .find('.product_image a')
+//           .attr('href')
+//         productDetails.productImgSrc = productElement
+//           .find('.product_image img')
+//           .attr('src')
+//         productDetails.productImgAlt = productElement
+//           .find('.product_image img')
+//           .attr('alt')
+//         productDetails.rating = productElement
+//           .find('.product_rating .rating5')
+//           .attr('title')
+//         productDetails.reviewCount = productElement
+//           .find('.product_review_count')
+//           .text()
+//           .replace(/[\(\)]/g, '')
+//           .trim()
+//         productDetails.price = productElement
+//           .find('.product_price')
+//           .text()
+//           .trim()
+//         productDetails.pricePerUnit = productElement
+//           .find('.price')
+//           .text()
+//           .trim()
+//         productDetails.productId = productElement.attr('data-productid')
+//         productDetails.objectId = productElement.attr('data-object-id')
+
+//         products.push(productDetails)
+//       })
+
+//       return products
+//     } catch (error) {
+//       console.error('Error fetching the URL:', error)
+//       return null
+//     }
+//   }
+
+//   fetchProductDetails(url).then((products) => {
+//     console.log(products)
+//   })
+// })
 module.exports = router
